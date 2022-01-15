3cc810b7f7d696591a56115c99681694f58232d4
zebus3d <zebus3dream@gmail.com>
2022-01-15 01:57:49 +0100

    mas jquery

--- js/main.js
+++ js/main.js
@@ -2,38 +2,28 @@
 $( document ).ready(function() {
     console.log( "ready!" );
 
-    var acc = document.getElementsByClassName("accordion");
-
-    // $("accordion").find(`[data-slide='${current}']`)
-
-    for (i = 0; i < acc.length; i++) {
-      acc[i].addEventListener("click", function() {
-
-        this.classList.toggle("active");
-        if (this.style.boxShadow != 'none') {
-
+    $('.accordion').each(function(i) {
+      this.addEventListener("click", function() {
+        $(this).toggleClass("active");
+        if ($(this).css('box-shadow') != 'none') {
           $(this).find('.maximizar').css("display", "none");
           $(this).find('.minimizar').css("display", "block");
-          this.style.borderRadius = '8px 8px 0px 0px';
-          this.style.boxShadow = 'none';
-          for (let i = 0; i < this.children.length; i++) {
-            for (let j = 0; j < this.children[i].length; j++) {
-              console.log(this);
-            }
-            this.children[i].style.color = '#fff';
-            this.children[i].style.textShadow = '3px 4px 6px #011828';
-          }
+          $(this).css('border-radius', '8px 8px 0px 0px');
+          $(this).css('box-shadow', 'none');
+          $(this.children).each(function(i) {
+            $(this).css('color', '#fff');
+            $(this).css('text-shadow', '3px 4px 6px #011828');
+          });
         } else {
           $(this).find('.maximizar').css("display", "block");
           $(this).find('.minimizar').css("display", "none");
-          this.style.borderRadius = '8px';
-          this.style.boxShadow = '5px 7px 5px #1114228f';
-          for (let i = 0; i < this.children.length; i++) {
-            this.children[i].style.color = '#373737';
-            this.children[i].style.textShadow = 'none';
-          }
+          $(this).css('border-radius', '8px');
+          $(this).css('box-shadow', '5px 7px 5px #1114228f');
+          $(this.children).each(function(i) {
+            $(this).css('color', '#373737');
+            $(this).css('text-shadow', 'none');
+          });
         }
-
         var panel = $(this).next();
         if ( panel.css('display') === 'flex') {
           panel.css('display', "none");
@@ -43,7 +33,7 @@ $( document ).ready(function() {
 
 
       });
-    }
+    });
 
 
 });
