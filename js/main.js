e43715ad57be35feda06f8cc5e27731aebb8f611
zebus3d <zebus3dream@gmail.com>
2023-03-27 13:14:50 +0200

    actualizando estilos

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
-$(document).ready(function() {
+$(document).ready(function () {
   console.log("ready!");
 
   var con_box_shadow = 'rgba(17, 20, 34, 0.56) 5px 7px 5px 0px';
   var sin_box_shadow = 'rgba(0, 0, 0, 0) 0px 0px 0px 0px';
+
+  var resalto_header_underline = 'solid 2px rgb(0, 231, 255)'
 
   function abrir_acordeon() {
     $('button > .font-effect-3d-float').css('color', '#373737');
@@ -10,15 +12,28 @@ $(document).ready(function() {
     $(this).find('.mostrar_menos').css("display", "block");
     $(this).css('border-radius', '8px 8px 0px 0px');
     $(this).css('box-shadow', 'none');
-    $(this).css('border-bottom', 'solid 1px #5fcef1');
-    $(this.children).each(function(i) {
+    $(this).css('border-bottom', resalto_header_underline);
+    $(this.children).each(function (i) {
       $(this).css('color', '#fff');
       $(this).css('text-shadow', '3px 4px 6px #011828');
       $(this).css('font-weight', 'bolder');
     });
     var panel = $(this).next();
+    var current_button = $(this).text();
+    // console.log(current_button);
+    if (current_button.indexOf("Portfolio") >= 0) {
+      custom_height = '60px';
+    };
+    if (current_button.indexOf("Social Networks") >= 0) {
+      custom_height = '275px';
+    };
+    if (current_button.indexOf("RBDLab") >= 0) {
+      custom_height = '260px';
+    };
+    // console.log(custom_height);
     if (panel.css('display') != 'flex') {
       panel.css('display', "flex");
+      panel.css('min-height', custom_height);
     }
   }
 
@@ -30,7 +45,7 @@ $(document).ready(function() {
     $('button > .font-effect-3d-float').css('font-weight', 'bolder');
     $(this).css('border-radius', '8px');
     $(this).css('box-shadow', '5px 7px 5px #1114228f');
-        $(this).css('border-bottom', 'none');
+    $(this).css('border-bottom', 'none');
     $(this).removeClass("active");
     var panel = $(this).next();
     if (panel.css('display') === 'flex') {
@@ -39,15 +54,15 @@ $(document).ready(function() {
   }
 
 
-  $('.accordion').each(function(i) {
+  $('.accordion').each(function (i) {
 
     $('button > .font-effect-3d-float').css("color", "#fff");
     $('button > .font-effect-3d-float').css('text-shadow', 'none');
     $('button > .font-effect-3d-float').css('font-weight', 'bolder');
 
-    this.addEventListener("click", function() {
+    this.addEventListener("click", function () {
 
-      $('.active').each(function(i) {
+      $('.active').each(function (i) {
         cerrar_acordeon.call(this);
       });
 
@@ -62,9 +77,9 @@ $(document).ready(function() {
   });
 
   // si pinchamos fuera del acordeon los cerramos
-  $(document).click(function(event) {
+  $(document).click(function (event) {
     if (!$(event.target).closest('.accordion').length) {
-      $('.active').each(function(i) {
+      $('.active').each(function (i) {
         cerrar_acordeon.call(this);
         $('button > .font-effect-3d-float').css('color', '#fff');
       });
